a2defc3c85551ca336cf96a636b7d43c3f7823ed
konglee <konglee873@gmail.com>
2016-09-20 09:15:39 +0800

    Initial Commit

---
+++ WKVSUIWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKVSUIWebView
+//
+//  Created by kong on 16/9/20.
+//  Copyright © 2016年 konglee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WKVSUIWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKVSUIWebView
+//
+//  Created by kong on 16/9/20.
+//  Copyright © 2016年 konglee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
